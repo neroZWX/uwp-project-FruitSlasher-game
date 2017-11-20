e60e232448a61c0f0c890eb62d113bb78509d28c
neroZWX <971370106@qq.com>
2017-11-20 23:46:34 +0000

    FruitSlasher

---
+++ FruitSlasher/Assets/Scrpits/UIStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIStart : MonoBehaviour {
+
+    private Button btnPlay;
+    private Button btnSound;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
